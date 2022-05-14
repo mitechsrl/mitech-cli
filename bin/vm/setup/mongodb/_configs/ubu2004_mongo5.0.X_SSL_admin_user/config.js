8178a6a2fbc316606f3d2e517c2f587fbcf29772
ivanvaccari <vaccari87@gmail.com>
2022-05-14 10:26:58 +0200

    Added node 18 and mongodb 5 setups

---
+++ bin/vm/setup/mongodb/_configs/ubu2004_mongo5.0.X_SSL_admin_user/config.js
@@ -0,0 +1,40 @@
+// the config of this directory is an item for inquirer list
+
+module.exports = {
+    name: 'Ubuntu 20.04, MongoDb 5.0.X (ssl/tls self signed, auth admin, auth user, support to onit-next/LB4 )',
+    value: {
+        questions: [
+            {
+                type: 'password',
+                name: 'adminPassword',
+                message: 'Password utente admin'
+            },
+            {
+                type: 'password',
+                name: 'adminPasswordConfirm',
+                message: 'Conferma password utente admin'
+            },
+            {
+                type: 'input',
+                name: 'userUsername',
+                message: 'Username utente per app'
+            },
+            {
+                type: 'password',
+                name: 'userPassword',
+                message: 'Password utente per app'
+            },
+            {
+                type: 'password',
+                name: 'userPasswordConfirm',
+                message: 'Conferma password utente app'
+            },
+            {
+                type: 'input',
+                name: 'mongoPath',
+                default: 'default',
+                message: 'Path storage (se disco aggiuntivo deve già essere montato, lassciare vuoto per default)?'
+            }
+        ]
+    }
+};
---
+++ bin/vm/setup/mongodb/_configs/ubu2004_mongo5.0.X_SSL_admin_user/index.js
@@ -0,0 +1,63 @@
+const path = require('path');
+
+const fs = require('fs');
+const logger = require('../../../../../../lib/logger');
+
+module.exports = (session, answers) => {
+
+    const jsFileContant = `
+    use admin;
+    db.createUser({
+        user: "admin",
+        pwd: "` + answers.adminPassword + `",
+        roles: [ { role: "root", db: "admin" }, "readWriteAnyDatabase" ]
+    })
+    db.changeUserPassword('admin','` + answers.adminPassword + `')
+
+    db.createUser({
+        user: "` + answers.userUsername + `",
+        pwd: "` + answers.userPassword + `",
+        roles: [{ role: "readWriteAnyDatabase", db: "admin" }]
+    });`;
+
+    logger.debug('Upload install_mongo.sh...');
+    const setupSh = path.join(__dirname, 'install_mongo.sh');
+    return session.command('sudo apt update')
+        .then(() => {
+            return session.command('sudo apt upgrade -y');
+        })
+        .then(() => {
+            // istall dos2unix per evitare problemi di \r\n su server quando si caricano i files creati da windows
+            logger.debug('Installo dos2unix...');
+            return session.command('sudo apt install dos2unix');
+        })
+        .then(() => {
+            logger.debug('Upload install_mongo.sh');
+            return session.uploadFile(setupSh, '/tmp/install_mongo.sh');
+        })
+        .then(() => {
+            logger.debug('Upload mongodbUsers.js');
+            fs.writeFileSync('mongodbUsers.js', jsFileContant);
+            return session.uploadFile('mongodbUsers.js', '/tmp/mongodbUsers.js')
+                .then(() => fs.unlinkSync('mongodbUsers.js'));
+        })
+        .then(() => session.command('dos2unix /tmp/install_mongo.sh'))
+        .then(() => session.command('dos2unix /tmp/mongodbUsers.js'))
+        .then(() => session.command('sudo chmod +x /tmp/install_mongo.sh'))
+        .then(() => {
+            logger.debug('Avvio setup...');
+
+            // escape for bash
+            const mongoPath = (answers.mongoPath || 'default').replace(/([^a-zA-Z0-9])/g, '\\' + '$1');
+
+            return session.command('sudo /tmp/install_mongo.sh "' + mongoPath + '"');
+        })
+        .then(() => {
+            logger.info('Setup completo!');
+        })
+        .catch(error => {
+            logger.error(error);
+        })
+        .then(() => session.command('rm /tmp/install_mongo.sh'))
+        .then(() => session.command('rm /tmp/mongodbUsers.js'));
+};
