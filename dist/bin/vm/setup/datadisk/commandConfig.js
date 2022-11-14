56d121e0bebb1d9ed2abab905b944a000189efdc
ivanvaccari <ivan.vaccari@mitechsrl.it>
2022-11-14 17:26:10 +0100

    Minor updates

--- dist/bin/vm/setup/datadisk/commandConfig.js
+++ dist/bin/vm/setup/datadisk/commandConfig.js
@@ -3,7 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const config = {
     description: 'Utility setup disco aggiuntivo su VM',
     longHelp: 'Questo comando formatta e monta un disco dati aggiuntivo su VM.\n' +
-        'E\' necessario aver già connesso il disco aggiuntivo alla macchina virtuale.',
+        'E\' necessario aver già connesso il disco aggiuntivo alla macchina virtuale.\n' +
+        'Automtizza questo documento: https://mitechsrlmn.sharepoint.com/:w:/s/Mitown/EUIADSbB9oZNj_-QGXfbyaUBxY9MBozrAUQMU3sT49d2Ew?e=zr8UxY',
     exec: './exec',
     params: []
 };
