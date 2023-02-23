87a22f7cc96aa7476821c9747729e03cf6e7022f
ivanvaccari <ivan.vaccari@mitechsrl.it>
2023-02-23 17:39:20 +0100

    Aggiunta utility generazione uuid

---
+++ dist/bin/uuid/exec.js
@@ -0,0 +1,25 @@
+"use strict";
+/**
+ * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ * Version 2, December 2004
+ * Copyright (C) 2004 Sam Hocevar
+ * 22 rue de Plaisance, 75014 Paris, France
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ * TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION:
+ * 0. You just DO WHAT THE FUCK YOU WANT TO.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const logger_1 = require("../../lib/logger");
+const uuid_1 = require("uuid");
+const exec = async (argv) => {
+    logger_1.logger.log((0, uuid_1.v4)());
+    logger_1.logger.log((0, uuid_1.v4)());
+    logger_1.logger.log((0, uuid_1.v4)());
+    logger_1.logger.log((0, uuid_1.v4)());
+};
+exports.default = exec;
+//# sourceMappingURL=exec.js.map
---
+++ src/bin/uuid/commandConfig.ts
@@ -0,0 +1,9 @@
+import { Command } from '../../types';
+
+const config: Command = {
+    description: 'Generatore uuid casuali',
+    exec: './exec',
+    params: []
+};
+
+export default config;
